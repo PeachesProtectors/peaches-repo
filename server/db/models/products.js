cfd8c8bbb1ff68cefc028ec65cd87fb8dadbb73e
Jennifer Chen <jenniferchen95@gmail.com>
2020-04-21 16:53:14 -0400

    feat(script/seed): added products, order seed data

--- server/db/models/products.js
+++ server/db/models/products.js
@@ -1,33 +1,33 @@
 const Sequelize = require('sequelize')
 const db = require('../db')
 
-const Products = db.define("product", {
-    name:{
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    description:{
-        type: Sequelize.TEXT,
-        allowNull: false
-    },
-    imageURL:{
-        type: Sequelize.TEXT,
-        allowNull: false
-    },
-    price:{
-        type: Sequelize.INTEGER,
-        validate:{
-            min: 0,
-            isDecimal: false
-        }
-    },
-    lightRequirement:{
-        type: Sequelize.STRING,
-        allowNull: false,
-        validate: {
-            isIn: [["Low Light", "Bright Light"]]
-        }
+const Products = db.define('product', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
+  imageUrl: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
+  price: {
+    type: Sequelize.INTEGER,
+    validate: {
+      min: 0,
+      isDecimal: false
     }
+  },
+  lightRequirement: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isIn: [['Low Light', 'Bright Light']]
+    }
+  }
 })
 
 module.exports = Products
