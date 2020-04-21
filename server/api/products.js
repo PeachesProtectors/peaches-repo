c476a4e4b0dc5a5bdf9371e12ab6647f353d1ca0
Jennifer Chen <jenniferchen95@gmail.com>
2020-04-21 17:05:51 -0400

    feat(server/api): added allproduct and singleproduct api routes

---
+++ server/api/products.js
@@ -0,0 +1,26 @@
+const router = require('express').Router()
+const {Product} = require('../db/models')
+
+module.exports = router
+
+router.get('/', async (req, res, next) => {
+  try {
+    const products = await Product.findAll()
+    res.json(products)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get('/:productId', async (req, res, next) => {
+  try {
+    const singleProduct = await Product.findOne({
+      where: {
+        id: req.params.productId
+      }
+    })
+    res.json(singleProduct)
+  } catch (err) {
+    next(err)
+  }
+})
