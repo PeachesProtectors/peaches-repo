750baee3c35e4589778dc4212894970a58ac5943
Linwei Liu <llwsgr@hotmail.com>
2020-04-21 21:55:13 -0400

    feat: add orderHistory model, update order model

--- script/seed.js
+++ script/seed.js
@@ -122,7 +122,23 @@ async function seed() {
     })
   ])
 
-  const orders = await Promise.all([Order.create({userId: 2})])
+  const orderHistroy = await Promise.all([
+    Order.create({
+      orderDate: '2016-08-09 04:05:02'
+    }),
+    Order.create({
+      orderDate: '2020-04-21 16:03:05'
+    })
+  ])
+
+  const orders = await Promise.all([
+    Order.create({
+      orderDate: '2016-08-09 04:05:02'
+    }),
+    Order.create({
+      orderDate: '2020-04-21 16:03:05'
+    })
+  ])
 
   console.log(`seeded ${users.length} users`)
   console.log(`seeded ${products.length} products`)
--- server/db/models/order.js
+++ server/db/models/order.js
@@ -2,7 +2,10 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const Order = db.define('order', {
-    
+  orderDate: {
+    type: Sequelize.DATE,
+    allowNull: false
+  }
 })
 
-module.exports = Order;
+module.exports = Order
