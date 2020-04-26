14c3c46c817f517e6e3908dda6e9a301730866b2
Nichole Cruzalegui <n.cruzalegui@hotmail.com>
2020-04-26 18:09:49 -0400

    debug: admin page add/update 2

--- client/components/admin.js
+++ client/components/admin.js
@@ -7,58 +7,14 @@ import {addPlantThunk} from '../store/admin'
  * COMPONENT
  */
 class Admin extends React.Component {
-  render() {
-    const {handleSubmit} = this.props
-    return (
-      <div>
-        <h2>Add Plant:</h2>
-        <form onSubmit={handleSubmit}>
-          <div>
-            <label htmlFor="plantName">
-              <small>Plant Name</small>
-            </label>
-            <input name="plantName" type="text" />
-          </div>
-
-          <div>
-            <label htmlFor="description">
-              <small>Description</small>
-            </label>
-            <input name="description" type="text" />
-          </div>
-
-          <div>
-            <label htmlFor="imageUrl">
-              <small>Photo</small>
-            </label>
-            <input name="imageUrl" type="text" />
-          </div>
-
-          <div>
-            <label htmlFor="price">
-              <small>Price</small>
-            </label>
-            <input name="price" type="price" />
-          </div>
-
-          <div>
-            <label htmlFor="lightRequirement">
-              <small>Light Requirements:</small>
-            </label>
-            <input list="lightRequirements" name="lightRequirements" />
-            <datalist id="lightRequirements">
-              <option value="Low Light" />
-              <option value="Bright Light" />
-            </datalist>
-          </div>
-
-          <div>
-            <button type="submit">Submit</button>
-          </div>
-        </form>
-      </div>
-    )
-  }
+  // render() {
+  // const {handleSubmit} = this.props
+  // return (
+  // <div>
+  //   <h2>Add Plant:</h2>
+  // </div>
+  // )
+  // }
 }
 
 const mapDispatch = dispatch => {
