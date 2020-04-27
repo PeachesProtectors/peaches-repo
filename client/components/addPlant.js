b2bb34273a2baf7a6f746db383d4af26cd18d4ad
Nichole Cruzalegui <n.cruzalegui@hotmail.com>
2020-04-27 14:50:38 -0400

    feat: addPlant local state

--- client/components/addPlant.js
+++ client/components/addPlant.js
@@ -14,6 +14,7 @@ class AddPlant extends React.Component {
   constructor() {
     super()
     this.state = initState
+    this.handleSubmit = this.handleSubmit.bind(this)
     this.handleChange = this.handleChange.bind(this)
   }
 
@@ -21,38 +22,78 @@ class AddPlant extends React.Component {
     this.setState({[e.target.name]: e.target.value})
   }
 
+  async handleSubmit(e) {
+    e.preventDefault()
+    try {
+      const name = e.target.name.value
+      const description = e.target.description.value
+      const imageUrl = e.target.imageUrl.value
+      const price = e.target.price.value
+      const lightRequirement = e.target.lightRequirements.value
+      const plant = {name, description, imageUrl, price, lightRequirement}
+
+      await this.props.addPlant(plant)
+      this.setState(initState)
+
+      document.getElementById(
+        'added-plant-message'
+      ).innerHTML = `<i>Hello ${name}!</i>`
+    } catch (error) {
+      console.error(error)
+    }
+  }
   render() {
-    const {handleSubmit} = this.props
     return (
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={this.handleSubmit}>
         <div>
           <label htmlFor="name">
             <small>Name</small>
           </label>
-          <input name="name" type="text" />
+          <input
+            onChange={this.handleChange}
+            name="name"
+            type="text"
+            value={this.state.name}
+          />
         </div>
         <div>
           <label htmlFor="description">
             <small>Description</small>
           </label>
-          <textarea name="description" rows="5" cols="50" />
+          <textarea
+            onChange={this.handleChange}
+            name="description"
+            rows="5"
+            cols="50"
+            value={this.state.description}
+          />
         </div>
         <div>
           <label htmlFor="imageUrl">
             <small>Picture</small>
           </label>
-          <input name="imageUrl" type="text" />
+          <input
+            onChange={this.handleChange}
+            name="imageUrl"
+            type="text"
+            value={this.state.imageUrl}
+          />
         </div>
         <div>
           <label htmlFor="price">
             <small>Price</small>
           </label>
-          <input name="price" type="price" />
+          <input
+            onChange={this.handleChange}
+            name="price"
+            type="text"
+            value={this.state.price}
+          />
         </div>
         <div>
           <label htmlFor="lightRequirements">
             <small>Light Requirements:</small>
-            <select name="lightRequirements">
+            <select name="lightRequirements" onChange={this.handleChange}>
               <option> - select light requirements - </option>
               <option value="Low Light">Low Light</option>
               <option value="Bright Light">Bright Light</option>
@@ -70,19 +111,7 @@ class AddPlant extends React.Component {
 
 const mapDispatch = dispatch => {
   return {
-    handleSubmit(e) {
-      e.preventDefault()
-      const name = e.target.name.value
-      const description = e.target.description.value
-      const imageUrl = e.target.imageUrl.value
-      const price = e.target.price.value
-      const lightRequirement = e.target.lightRequirements.value
-      const plantObj = {name, description, imageUrl, price, lightRequirement}
-      document.getElementById(
-        'added-plant-message'
-      ).innerHTML = `<i>Hello ${name}!</i>`
-      dispatch(addPlantThunk(plantObj))
-    }
+    addPlant: plant => dispatch(addPlantThunk(plant))
   }
 }
 
